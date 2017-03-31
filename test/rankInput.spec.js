66a48c215e3c5fe673f0959d743f0bcd26dc0631
Doug VonMoser <dvonmoser@cars.com>
2017-03-31 17:33:46 -0500

    added a basic test for ranking input

--- test/rankInput.spec.js
+++ test/rankInput.spec.js
@@ -1,2 +1,12 @@
 const expect = require('chai').expect;
 const rankInput = require('../src/helpers/rankInput');
+
+const preNodeArray = require('../fixtures/preDescribedNodeArray.js');
+const postNodeArray = require('../fixtures/postDescribedNodeArray.js');
+
+describe('prepare input', function(){
+    let response = rankInput(preNodeArray);
+    it('should do its damn job', function(){
+       expect(response).to.deep.equal(postNodeArray);
+    });
+});
